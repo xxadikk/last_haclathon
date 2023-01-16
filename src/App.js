63df93f26f6cd107b0b29ef890aafd02fd55e736
unknown <alisherrov1@gmail.com>
2023-01-16 12:15:45 +0600

    проверка

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Routing from "./Routing";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routing />
+    </>
+  );
 };
 
 export default App;
--- src/Routing.js
+++ src/Routing.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import AboutUs from "./components/pages/AboutUs";
 
 const Routing = () => {
   const PRIVAT_PAGES = [];
-  const PUBLIC_PAGES = [];
-  return <Routes></Routes>;
+  const PUBLIC_PAGES = [
+    {
+      link: "/about",
+      element: <AboutUs />,
+      id: 1,
+    },
+  ];
+  return (
+    <Routes>
+      {PUBLIC_PAGES.map((page) => (
+        <Route path={page.link} key={page.id} element={page.element} />
+      ))}
+    </Routes>
+  );
 };
 
 export default Routing;
