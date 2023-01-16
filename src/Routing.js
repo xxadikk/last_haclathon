03a3e94424c3c17d633a041f54cf02c42e721238
unknown <alisherrov1@gmail.com>
2023-01-16 11:41:14 +0600

    first commit

---
+++ src/Routing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+const Routing = () => {
+  const PRIVAT_PAGES = [];
+  const PUBLIC_PAGES = [];
+  return <Routes></Routes>;
+};
+
+export default Routing;
