82bfe9cb60c39fcf0a3b943d629e7da8b392d956
unknown <alisherrov1@gmail.com>
2023-01-18 16:57:51 +0600

    samat pull

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,8 +7,6 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import "../Navbar/Navbar.css";
@@ -17,11 +15,12 @@ import HomeIcon from "@mui/icons-material/Home";
 import StorefrontIcon from "@mui/icons-material/Storefront";
 import InfoIcon from "@mui/icons-material/Info";
 
-const settings = ["Logout", "Register"];
+import MenuRegister from "./MenuRegister/MenuRegister";
+
 
 function Navbar() {
   const navigate = useNavigate();
-
+  
   const pages = [
     {
       name: "Главная",
@@ -76,8 +75,8 @@ function Navbar() {
   };
 
   return (
-    <Box id = "start-container" sx ={{ width: "100%,", padding:"0"}}>
-    <AppBar id="appbar" position="fixed" sx={{width: "100%", top: '15px'}}>
+    <Box id = "start-container" sx ={{ width: "100%,", padding:"0",}}>
+    <AppBar id="appbar" position="fixed" sx={{width: "100%",}}>
       <Container maxWidth="100%" >
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
@@ -189,36 +188,7 @@ function Navbar() {
                 </>
               ))}
             </Box>
-
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
+            <MenuRegister/>
           </Toolbar>
         </Container>
       </AppBar>
